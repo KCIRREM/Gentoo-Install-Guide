6bb3c4afaf410965e9bdec32378fded54b83002d
KCIRREM <102417215+KCIRREM@users.noreply.github.com>
2025-07-06 12:46:28 +0000

    Update automount.sh

--- configs/automount.sh
+++ configs/automount.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
+
 current_path_abs=$(realpath $(dirname $0))
+mkdir -p /mnt/gentoo
 mount -o defaults,noatime,compress-force=zstd,subvol=@ /dev/mapper/cryptroot /mnt/gentoo/
-mkdir -p /mnt/gentoo/{home,.snapshots,var,efi}
-mkdir -p /mnt/gentoo/var/{cache,db,log,tmp}
-mkdir -p /mnt/gentoo/var/db/repos
-mkdir -p /mnt/gentoo/var/cache/binpkgs
+
+mkdir  /mnt/gentoo/{home,.snapshots,var,efi}
+mkdir  /mnt/gentoo/var/{cache,db,log,tmp}
+mkdir  /mnt/gentoo/var/db/repos
+
 mount /dev/nvme0n1p1 /mnt/gentoo/efi
 mount -o defaults,noatime,compress-force=zstd,subvol=@home /dev/mapper/cryptroot /mnt/gentoo/home
 mount -o defaults,noatime,compress-force=zstd,subvol=@snapshots /dev/mapper/cryptroot /mnt/gentoo/.snapshots
@@ -12,10 +15,14 @@ mount -o defaults,noatime,compress-force=zstd,subvol=@tmp /dev/mapper/cryptroot 
 mount -o defaults,noatime,compress-force=zstd,subvol=@log /dev/mapper/cryptroot /mnt/gentoo/var/log
 mount -o defaults,noatime,compress-force=zstd,subvol=@cache /dev/mapper/cryptroot /mnt/gentoo/var/cache
 mount -o defaults,noatime,compress-force=zstd,subvol=@repos /dev/mapper/cryptroot /mnt/gentoo/var/db/repos
+mkdir /mnt/gentoo/var/cache/binpkgs
 mount -o defaults,noatime,compress-force=zstd,subvol=@binpkgs /dev/mapper/cryptroot /mnt/gentoo/var/cache/binpkgs
 
+
+pwd=$(pwd)
 wget https://distfiles.gentoo.org/releases/amd64/autobuilds/20250702T205201Z/stage3-amd64-hardened-openrc-20250702T205201Z.tar.xz
-mv stage3-*.tar.xz /mnt/gentoo/*
+cd $pwd
+mv stage3-*.tar.xz /mnt/gentoo/
 tar xpvf /mnt/gentoo/stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner -C /mnt/gentoo
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 mount --types proc /proc /mnt/gentoo/proc
@@ -28,5 +35,3 @@ mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm
 chmod 1777 /dev/shm /run/shm
 cp -r $current_path_abs /mnt/gentoo/
 chroot /mnt/gentoo /bin/bash 
-source /etc/profile 
-export PS1="(chroot) ${PS1}"
